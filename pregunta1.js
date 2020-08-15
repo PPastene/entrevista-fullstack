85e483512a4ba0e020483502eca0a98938fe6512
Pedro Pablo Pastene <pe.pastene@gmail.com>
2020-08-15 18:56:12 -0400

    Ejercicio resuelto

--- pregunta1.js
+++ pregunta1.js
@@ -4,7 +4,25 @@
  * @param {number[]} integers 
  */
 function findOutlier(integers) {
+    // Verificacion de que el arreglo tenga 3 elementos minimo
+    if(integers.length < 3) { return false }
+
+    var pares = []
+    var impares = []
+
+    // Se compara cada elemento del arreglo
+    // El numero se movera depeniendo si el resto de la division sea cero (par)
+    integers.forEach(numero => {
+        if (numero % 2 == 0) { pares.push(numero) }
+        else { impares.push(numero) }
+    });
+
+    // Se comparan ambos arreglos para ver cual de ellos solo tiene 1 elemento que corresponde al 'outlier'
+    // Si ambos no tienen solo 1 elemento el 'outlier' no existe en el arreglo original
+    if(pares.length == 1) { return pares[0] }
+    else if(impares.length == 1) {  return impares[0] }
     
+    return false
 }
 
 
